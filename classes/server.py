c831f16590b51f836431f625d3b9b4742fbf23a1
VladimirSuddenok <suddenokvg@yandex.ru>
2021-02-26 16:54:49 +0300

    added base server class

---
+++ classes/server.py
@@ -0,0 +1,42 @@
+from classes.parents_classes.base_class import BaseClass
+from aiohttp import web
+
+class Server(BaseClass):
+    parameters = ["host", "port"]
+
+    def __init__(self):
+        '''  '''
+        # here will be all application routes
+        # type: get, post, put, view
+        # template {"url": "your/url", "type": "view", "handler": your_handel}
+        self._routes = [
+
+        ]
+
+    def _set_view(self, app) -> int:
+        ''' build application routes '''
+        for rout in self._routes:
+            buffer = []
+
+            if route["type"] == "view":
+                buffer.append(
+                    web.view(route["url"], route["handler"])
+                )
+
+            # elif pass
+                
+            app.add_routes(buffer)
+        
+        return 0
+
+    @classmethod
+    def get_runner(cls):
+        ''' return coroutine for start server '''
+        instance = Server()
+        app = web.Application()
+
+        instance._set_view(app=app)
+        run_args = {"app": app, "host": cls.host, "port": cls.port}
+
+        return web._run_app(**run_args)
+        #app.add_routes([web.get('/', hello)])
