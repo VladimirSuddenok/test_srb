f7ee5f4d7d0c49e62823805907741caaf203898c
VladimirSuddenok <suddenokvg@yandex.ru>
2021-02-27 22:58:41 +0300

    added new test runners

--- run_test_app.py
+++ run_test_app.py
@@ -2,11 +2,54 @@
 
 import asyncio
 #from settings import Setup
-from classes.server import Server 
+from classes.server import Server
+from classes.backed_connectors import SQLDB
 
-version = "0.001.001"
+version = "0.002.001"
 
-async def printer():
+async def test_run_server():
+    settings = {"host": "127.0.0.1", "port": 8080}
+    Server.set_cls_params(**settings)
+    server_runner = Server.get_runner()
+    print("server_run_cor", server_runner)
+    await server_runner
+
+    return 0
+
+async def test_sql_connection():
+    settings = {
+        "connection_string": r"sqlite:///C:\\Users\\Владимир\\Desktop\\сбер\\foo.db",
+        "pool_size": 3,
+        "max_overflow": 0
+    }
+    #SQLBD.set_cls_params(**settings)
+    sql = SQLDB(meth="init", params=settings)
+    show_table = '''
+    SELECT 
+        name
+    FROM 
+        sqlite_master 
+    WHERE 
+        type ='table' AND 
+        name NOT LIKE 'sqlite_%';
+    '''
+    test_query = '''
+        select * from test_second;
+    '''
+
+    print(await sql.execute_query(query=test_query))
+    #print ("sql", sql)
+    #print("sql", sql.__dict__)
+    #sql = SQLBD()
+    #print ("sql", sql)
+    ##Server.set_cls_params(**settings)
+    ##server_runner = Server.get_runner()
+    #print("sql", sql.__dict__)
+    #await server_runner
+
+    return 0
+
+async def test_no_sql_connection():
     settings = {"host": "127.0.0.1", "port": 8080}
     Server.set_cls_params(**settings)
     server_runner = Server.get_runner()
@@ -23,7 +66,7 @@ async def main():
     #start_func, args = server.get_runner()
     
     await asyncio.gather(
-        printer()
+        test_sql_connection()
         # print start log
         # run server
     )
